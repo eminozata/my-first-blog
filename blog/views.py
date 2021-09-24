2bc81f546a0a3968ebf51900d00cfa0127a19f5f
eminozata <77062312+eminozata@users.noreply.github.com>
2021-09-24 14:05:25 +0300

    delete test.html

--- blog/views.py
+++ blog/views.py
@@ -11,8 +11,8 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def test(request):
-
-    return render(request, 'blog/test.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/test.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
